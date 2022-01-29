c2ae4c418b379b7194b1082094db4dfa673355cf
klusht <klusht@gmail.com>
2022-01-29 16:09:15 +0000

    Updated deployment for containerd and k8s 1.23

--- 01_build_hosts_file.sh
+++ 01_build_hosts_file.sh
@@ -1,248 +1,64 @@
 #!/bin/bash
 
+# GET yq binary -------------------------------
 echo "Building hosts.yaml file"
 if [ ! -f local_resources/yq ];then
-  mkdir -p local_resources
-  wget -cO - https://github.com/mikefarah/yq/releases/download/3.3.2/yq_linux_amd64 > local_resources/yq && chmod +x local_resources/yq
+  mkdir -p local_resources; wget -cO - https://github.com/mikefarah/yq/releases/download/3.3.2/yq_linux_amd64 > local_resources/yq && chmod +x local_resources/yq
 fi
 yq() { local_resources/yq "$@"; }; export -f yq
+# end GET yq binary ---------------------------
 
+# Function to build yaml tree -------------------------------
 function create_host_entries() {
-  group=$1
-  node=$2
-  node_ip=$3
-  yq w -i hosts.yaml $group.hosts.$node.ansible_host $node_ip
-  yq w -i hosts.yaml $group.hosts.$node.kubernetes.k8s_install_version "$k8s_install_version"
-  yq w -i hosts.yaml $group.hosts.$node.kubernetes.docker_install_version "$docker_install_version"
+  group=$1; node=$2; node_ip=$3
+  yq w -i hosts.yaml $group.hosts.$node.ansible_host $node_ip; yq w -i hosts.yaml $group.hosts.$node.kubernetes.k8s_install_version "$k8s_install_version"
+  yq w -i hosts.yaml $group.hosts.$node.kubernetes.container_runtimes_interface "$container_runtimes_interface";  yq w -i hosts.yaml $group.hosts.$node.kubernetes.cri_install_version "$cri_install_version"
 }
-
 function create_nodes() {
-  nodepool=$1
-  ips=$2
-  for i in "${!ips[@]}"; do
-    node_dn="${cluster_name}-worker-${nodepool}-$i"
-    create_host_entries workervms $node_dn ${ips[$i]}
-  done
+  nodepool=$1;  ips=$2; for i in "${!ips[@]}"; do node_dn="${cluster_name}-worker-${nodepool}-$i";  create_host_entries workervms $node_dn ${ips[$i]};  done
 }
+# END Function to build yaml tree -------------------------------
 
-###################################
 
-#k8s_install_version=1.5.4-0
-#k8s_install_version=1.5.4-1
-#k8s_install_version=1.6.0-0
-#k8s_install_version=1.6.0-1
-#k8s_install_version=1.6.1-0
-#k8s_install_version=1.6.1-1
-#k8s_install_version=1.6.2-0
-#k8s_install_version=1.6.2-1
-#k8s_install_version=1.6.3-0
-#k8s_install_version=1.6.3-1
-#k8s_install_version=1.6.4-0
-#k8s_install_version=1.6.4-1
-#k8s_install_version=1.6.5-0
-#k8s_install_version=1.6.5-1
-#k8s_install_version=1.6.6-0
-#k8s_install_version=1.6.6-1
-#k8s_install_version=1.6.7-0
-#k8s_install_version=1.6.7-1
-#k8s_install_version=1.6.8-0
-#k8s_install_version=1.6.8-1
-#k8s_install_version=1.6.9-0
-#k8s_install_version=1.6.9-1
-#k8s_install_version=1.6.10-0
-#k8s_install_version=1.6.10-1
-#k8s_install_version=1.6.11-0
-#k8s_install_version=1.6.11-1
-#k8s_install_version=1.6.12-0
-#k8s_install_version=1.6.12-1
-#k8s_install_version=1.6.13-0
-#k8s_install_version=1.6.13-1
-#k8s_install_version=1.7.0-0
-#k8s_install_version=1.7.0-1
-#k8s_install_version=1.7.1-0
-#k8s_install_version=1.7.1-1
-#k8s_install_version=1.7.2-0
-#k8s_install_version=1.7.2-1
-#k8s_install_version=1.7.3-1
-#k8s_install_version=1.7.3-2
-#k8s_install_version=1.7.4-0
-#k8s_install_version=1.7.4-1
-#k8s_install_version=1.7.5-0
-#k8s_install_version=1.7.5-1
-#k8s_install_version=1.7.6-1
-#k8s_install_version=1.7.6-2
-#k8s_install_version=1.7.7-1
-#k8s_install_version=1.7.7-2
-#k8s_install_version=1.7.8-1
-#k8s_install_version=1.7.8-2
-#k8s_install_version=1.7.9-0
-#k8s_install_version=1.7.9-1
-#k8s_install_version=1.7.10-0
-#k8s_install_version=1.7.10-1
-#k8s_install_version=1.7.11-0
-#k8s_install_version=1.7.11-1
-#k8s_install_version=1.7.14-0
-#k8s_install_version=1.7.15-0
-#k8s_install_version=1.7.16-0
-#k8s_install_version=1.8.0-0
-#k8s_install_version=1.8.0-1
-#k8s_install_version=1.8.1-0
-#k8s_install_version=1.8.1-1
-#k8s_install_version=1.8.2-0
-#k8s_install_version=1.8.2-1
-#k8s_install_version=1.8.3-0
-#k8s_install_version=1.8.3-1
-#k8s_install_version=1.8.4-0
-#k8s_install_version=1.8.4-1
-#k8s_install_version=1.8.5-0
-#k8s_install_version=1.8.5-1
-#k8s_install_version=1.8.6-0
-#k8s_install_version=1.8.7-0
-#k8s_install_version=1.8.8-0
-#k8s_install_version=1.8.9-0
-#k8s_install_version=1.8.10-0
-#k8s_install_version=1.8.11-0
-#k8s_install_version=1.8.12-0
-#k8s_install_version=1.8.13-0
-#k8s_install_version=1.8.14-0
-#k8s_install_version=1.8.15-0
-#k8s_install_version=1.9.0-0
-#k8s_install_version=1.9.1-0
-#k8s_install_version=1.9.2-0
-#k8s_install_version=1.9.3-0
-#k8s_install_version=1.9.4-0
-#k8s_install_version=1.9.5-0
-#k8s_install_version=1.9.6-0
-#k8s_install_version=1.9.7-0
-#k8s_install_version=1.9.8-0
-#k8s_install_version=1.9.9-0
-#k8s_install_version=1.9.10-0
-#k8s_install_version=1.9.11-0
-#k8s_install_version=1.10.0-0
-#k8s_install_version=1.10.1-0
-#k8s_install_version=1.10.2-0
-#k8s_install_version=1.10.3-0
-#k8s_install_version=1.10.4-0
-#k8s_install_version=1.10.5-0
-#k8s_install_version=1.10.6-0
-#k8s_install_version=1.10.7-0
-#k8s_install_version=1.10.8-0
-#k8s_install_version=1.10.9-0
-#k8s_install_version=1.10.10-0
-#k8s_install_version=1.10.11-0
-#k8s_install_version=1.10.12-0
-#k8s_install_version=1.10.13-0
-#k8s_install_version=1.11.0-0
-#k8s_install_version=1.11.1-0
-#k8s_install_version=1.11.2-0
-#k8s_install_version=1.11.3-0
-#k8s_install_version=1.11.4-0
-#k8s_install_version=1.11.5-0
-#k8s_install_version=1.11.6-0
-#k8s_install_version=1.11.7-0
-#k8s_install_version=1.11.8-0
-#k8s_install_version=1.11.9-0
-#k8s_install_version=1.11.10-0
-#k8s_install_version=1.12.0-0
-#k8s_install_version=1.12.1-0
-#k8s_install_version=1.12.2-0
-#k8s_install_version=1.12.3-0
-#k8s_install_version=1.12.4-0
-#k8s_install_version=1.12.5-0
-#k8s_install_version=1.12.6-0
-#k8s_install_version=1.12.7-0
-#k8s_install_version=1.12.8-0
-#k8s_install_version=1.12.9-0
-#k8s_install_version=1.12.10-0
-#k8s_install_version=1.13.0-0
-#k8s_install_version=1.13.1-0
-#k8s_install_version=1.13.2-0
-#k8s_install_version=1.13.3-0
-#k8s_install_version=1.13.4-0
-#k8s_install_version=1.13.5-0
-#k8s_install_version=1.13.6-0
-#k8s_install_version=1.13.7-0
-#k8s_install_version=1.13.8-0
-#k8s_install_version=1.13.9-0
-#k8s_install_version=1.13.10-0
-#k8s_install_version=1.13.11-0
-#k8s_install_version=1.13.12-0
-#k8s_install_version=1.14.0-0
-#k8s_install_version=1.14.1-0
-#k8s_install_version=1.14.2-0
-#k8s_install_version=1.14.3-0
-#k8s_install_version=1.14.4-0
-#k8s_install_version=1.14.5-0
-#k8s_install_version=1.14.6-0
-#k8s_install_version=1.14.7-0
-#k8s_install_version=1.14.8-0
-#k8s_install_version=1.14.9-0
-#k8s_install_version=1.14.10-0
-#k8s_install_version=1.15.0-0
-#k8s_install_version=1.15.1-0
-#k8s_install_version=1.15.2-0
-#k8s_install_version=1.15.3-0
-#k8s_install_version=1.15.4-0
-#k8s_install_version=1.15.5-0
-#k8s_install_version=1.15.6-0
-#k8s_install_version=1.15.7-0
-#k8s_install_version=1.15.8-0
-#k8s_install_version=1.15.9-0
-#k8s_install_version=1.15.10-0
-#k8s_install_version=1.15.11-0
-#k8s_install_version=1.15.12-0
-#k8s_install_version=1.16.0-0
-#k8s_install_version=1.16.1-0
-#k8s_install_version=1.16.2-0
-#k8s_install_version=1.16.3-0
-#k8s_install_version=1.16.4-0
-#k8s_install_version=1.16.5-0
-#k8s_install_version=1.16.6-0
-#k8s_install_version=1.16.7-0
-#k8s_install_version=1.16.8-0
-#k8s_install_version=1.16.9-0
-#k8s_install_version=1.16.10-0
-#k8s_install_version=1.16.11-0
-#k8s_install_version=1.16.11-1
-#k8s_install_version=1.16.12-0
-#k8s_install_version=1.16.13-0
-#k8s_install_version=1.16.14-0
-#k8s_install_version=1.16.15-0
-#k8s_install_version=1.17.0-0
-#k8s_install_version=1.17.1-0
-#k8s_install_version=1.17.2-0
-#k8s_install_version=1.17.3-0
-#k8s_install_version=1.17.4-0
-#k8s_install_version=1.17.5-0
-#k8s_install_version=1.17.6-0
-#k8s_install_version=1.17.7-0
-#k8s_install_version=1.17.7-1
-#k8s_install_version=1.17.8-0
-#k8s_install_version=1.17.9-0
-#k8s_install_version=1.17.11-0
-#k8s_install_version=1.17.12-0
-#k8s_install_version=1.18.0-0
-#k8s_install_version=1.18.1-0
-#k8s_install_version=1.18.2-0
-#k8s_install_version=1.18.3-0
-#k8s_install_version=1.18.4-0
-#k8s_install_version=1.18.4-1
-#k8s_install_version=1.18.5-0
-#k8s_install_version=1.18.6-0
-#k8s_install_version=1.18.8-0
-#k8s_install_version=1.18.9-0
-#k8s_install_version=1.19.0-0
-#k8s_install_version=1.19.1-0
-k8s_install_version=1.19.2-0
 
+
+
+# TODO support older version
+#k8s_install_version=1.12.10
+#k8s_install_version=1.14.10
+#k8s_install_version=1.15.12
+#k8s_install_version=1.16.15
+#k8s_install_version=1.17.12
+#k8s_install_version=1.18.9
+#k8s_install_version=1.19.2
+#k8s_install_version=1.20.14
+#k8s_install_version=1.21.8
+#k8s_install_version=1.22.5
+k8s_install_version=1.23.3                          # get kubectl > curl -LO https://dl.k8s.io/release/v1.23.0/bin/linux/amd64/kubectl
+
+
+container_runtimes_interface=containerd            # apt-cache madison containerd  OR apt-cache policy containerd
+cri_install_version=1.5.5-0ubuntu3~20.04.1
+#cri_install_version=1.5.2-0ubuntu1~20.04.3
+
+
+
+# TODO not tested with current 1.20  deployments
+#container_runtimes_interface=docker
+#docker_install_version=19.03.5
+
+
+
+# CLUSTER VARIABLES
 cluster_name=web-cluster
-docker_install_version=19.03.5
 service_cidr="10.100.0.0/12"
 #    pod_network= flannel
 pod_network=calico
 pod_network_cidr="10.200.0.0/16"
 schedule_pods_on_master=no
 
+#Clean hosts file
+echo "" > hosts.yaml
 
 master_dn="${cluster_name}-master-node"
 master_ip=192.168.0.100
@@ -253,7 +69,6 @@ yq w -i hosts.yaml mastervms.hosts.$master_dn.kubernetes.pod_network_cidr "$pod_
 yq w -i hosts.yaml mastervms.hosts.$master_dn.kubernetes.schedule_pods_on_master "$schedule_pods_on_master"
 
 
-
 #ips=("192.168.0.101" "192.168.0.102")
 ##ips=("192.168.0.101")
 #create_nodes large $ips
@@ -261,11 +76,9 @@ yq w -i hosts.yaml mastervms.hosts.$master_dn.kubernetes.schedule_pods_on_master
 ips=("192.168.0.102" "192.168.0.103")
 create_nodes medium $ips
 
-
 #ips=("192.168.0.105" "192.168.0.106")
 ##ips=("192.168.0.103")
 #create_nodes small $ips
-
 
 echo "done"
 
--- README.md
+++ README.md
@@ -1,23 +1,37 @@
-### Ansible project for setting up a kubernetes cluster on manual defined VM's using kubeadm
+## Ansible project for setting up a kubernetes cluster predefined VM's using kubeadm
+This project fallows the bare metal installation of kubernetes on VMs.
+Suitable for home user with windows workstations that wish to spawn up a k8s cluster similar to a cloud provider. 
 
 ### Prerequisites
 
-##### Configure kubernetes versions
-The current project assumes there are CentOS(7) VM's, as many actions are from the official installation guides for this OS.
+##### Configure virtual machines
+Currently, supported OS:
+OS-Ubuntu-20.04 (server)
+
+There are scripts available to clone a base image of vmware workstation player hypervisor similar to node-pools cloud definitions. 
+The scripts will set the CPU Cores, Memory and NIC MAC address in bridge mode, so you can reserve those IPs in your router.
+
+
+###How to get your vmware images:
+Simply create one image as a base image and provide the name of the folder in 00_vmware-player_win_create_cluster.cmd
+The image should have:
+python3 installed
+a user created in sudoers called "user" ( change it in  ansible.cfg remote_user )
+
 
 ##### Kubernetes configuration 
 - Update details in build_hosts_file.sh , and execute it to create the hosts.yaml file.
 
 #### Local configuration
 - Have all your VM's ready on your local hypervisor
-  - I use vmware player on windows and clone them on different HDD's to simulate different IO speeds
+  - I use vmware player on Windows and clone them on different HDD's to simulate different IO speeds
   - Use the script 00_vmware-player_win_create_cluster.cmd to automate the vms on Windows with VMware Player.
     - How the script works. It looks for a folder that has the copy of a base image. It copies the vmware image into multiple folders to reflect a pool config.
     Change the number of instances in the "for" loop to have more instances of the same VM. It also creates shortcuts to allow you to start all of them at once.
     Every time you need to crate a new cluster, run the script that will recreate the entire folder.
       Use reserved IP addresses on your router.
   - Make sure you use the same base image with the same root password
-  - Get all VMs assigned IP addresses and update the 0_build_hosts_file.sh . Hope the script is readable.
+  - Get all VMs assigned IP addresses and update the 01_build_hosts_file.sh . Hope the script is readable.
 
 
 ### USAGE
@@ -31,24 +45,9 @@ The current project assumes there are CentOS(7) VM's, as many actions are from t
        
 - You can create group_vars/mastervms.yaml and workervms.yaml Containing
   ansible_password: "your_root_password"
+- ansible_become_password: "your_user_password"
 ```
 add the IPs to build_hosts_file.sh
 run build_hosts_file.sh  to create hosts.yaml file   or skip this step if you alreay have them set up
 ./deploy_k8s.yaml 
 ```
-
-  #### How were the playbooks build
-- They mostly fallow the steps defined in the official guides 
-  - https://docs.docker.com/install/linux/docker-ce/centos/
-  - https://kubernetes.io/docs/setup/independent/install-kubeadm/
-  - https://kubernetes.io/docs/setup/independent/create-cluster-kubeadm/
-  
-#### Comments
-I did not use vagrant to initialize VM on any hypervisor ( virtualbox/vmware ) as there is a clear dependency to have first network interface as NAT, 
-which affects the kubeadm cluster installation being the primary NIC and the resulting IP not being accessible from outside.
-
-#### PROXMOX 
-Recently tested proxmox on winOS. I find it to be the best solution for cluster automation. 
-It provides a rich automation interface using their qm cli and a very useful UI. There is a separate branch containing this variant.
-if you receive CPU Locked on the nested VM, lower the SHARED CPU cores on the proxmox guest VM.
-
